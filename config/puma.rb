5f7318125779e0ea453fffb19a09e4fc527da25e
matthew-elia <m4tth3w.3l1a@gmail.com>
2017-01-12 18:18:45 -0500

    aws bs

--- config/puma.rb
+++ config/puma.rb
@@ -11,12 +11,13 @@ threads threads_count, threads_count
 #
 port        ENV.fetch("PORT") { 3000 }
 
+bind "unix:///var/run/puma/blankspace.sock"
+pidfile "/var/run/puma/blankspace.sock"
+
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-# bind "unix:///var/run/puma/my_app.sock"
-# pidfile "/var/run/puma/my_app.sock"
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
