989609c01af34f5a4ba72a531881ea1afb9cb65b
matthew-elia <matt.elia@gramercytech.com>
2017-02-04 09:54:11 -0500

    namespace refactor / port landing page design -> sidemenu (phase1) / bunch of minor adj

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 	root to: 'home#index'
-	get 'bio', controller: 'bio', action: 'index'
+	get 'life', controller: 'life', action: 'index'
 	get 'work', controller: 'work', action: 'index'
 	get 'work/:work_item', controller: 'work', action: 'work_item', as: 'work_item'
-	# get 'contact', controller: 'contact', action: 'index'
 end
